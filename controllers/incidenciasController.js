204b46f4f68284f183b8c9255e5840c576c59ad1
DiegoAramisX9 <00090225@uca.edu.sv>
2026-09-16 23:08:37 -0600

    Implementa obtenerClasificacion usando switch

--- controllers/incidenciasController.js
+++ controllers/incidenciasController.js
@@ -115,11 +115,40 @@ function obtenerEstadisticas(req, res) {
     });
 }
 
+// 8. Clasificación automática (obligatorio usar exclusivamente switch)
+function obtenerClasificacion(req, res) {
+    const id = Number(req.params.id);
+    const incidencia = incidencias.find((incidencia) => incidencia.id === id);
+
+    if (!incidencia) {
+        return res.status(404).json({ mensaje: 'Incidencia no encontrada' });
+    }
+
+    let clasificacion;
+
+    switch (incidencia.prioridad) {
+        case 'Alta':
+            clasificacion = 'Crítica';
+            break;
+        case 'Media':
+            clasificacion = 'Importante';
+            break;
+        case 'Baja':
+            clasificacion = 'Normal';
+            break;
+        default:
+            clasificacion = 'Sin clasificar';
+    }
+
+    res.json({ id: incidencia.id, clasificacion: clasificacion });
+}
+
 module.exports = {
     crearIncidencia,
     listarIncidencias,
     obtenerIncidenciaPorId,
     cambiarEstadoIncidencia,
     eliminarIncidencia,
-    obtenerEstadisticas
+    obtenerEstadisticas,
+    obtenerClasificacion
 };

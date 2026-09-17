6f41ead58c402ea148d7b3bc6d829c08443b576f
DiegoAramisX9 <00090225@uca.edu.sv>
2026-09-16 23:09:05 -0600

    Agrega rutas GET /estadisticas y GET /incidencias/:id/clasificacion

--- routes/incidencias.js
+++ routes/incidencias.js
@@ -4,13 +4,20 @@ const router = express.Router();
 const {
     crearIncidencia,
     listarIncidencias,
-    obtenerIncidenciaPorId
+    obtenerIncidenciaPorId,
+    cambiarEstadoIncidencia,
+    eliminarIncidencia,
+    obtenerEstadisticas,
+    obtenerClasificacion
 } = require('../controllers/incidenciasController');
 
 router.post('/incidencias', crearIncidencia);
 router.get('/incidencias', listarIncidencias);
+router.get('/incidencias/:id/clasificacion', obtenerClasificacion);
 router.get('/incidencias/:id', obtenerIncidenciaPorId);
 router.put('/incidencias/:id/estado', cambiarEstadoIncidencia);
 router.delete('/incidencias/:id', eliminarIncidencia);
+
+router.get('/estadisticas', obtenerEstadisticas);
 
 module.exports = router;
